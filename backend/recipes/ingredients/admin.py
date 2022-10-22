9158dad594b7b5665f6ce1193c4716a2d5824d38
Vladislav Gerasimov <Vladislav.Gerasimov@kaspersky.com>
2022-10-22 23:39:31 +0300

    Изменение админки для ингредиента

--- backend/recipes/ingredients/admin.py
+++ backend/recipes/ingredients/admin.py
@@ -5,5 +5,6 @@ from recipes.ingredients.models import Ingredient
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    # list_display = ()
-    pass
+    list_display = ('pk', 'name', 'measurement_unit',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
