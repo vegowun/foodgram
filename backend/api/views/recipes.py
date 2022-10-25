be8fe363e45fb033d438e97a4c51578db338b6a2
Vladislav Gerasimov <Vladislav.Gerasimov@kaspersky.com>
2022-10-26 01:30:22 +0300

    Добавление простого permission для рецептов

--- backend/api/views/recipes.py
+++ backend/api/views/recipes.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from api.pagination import LimitPageNumberPagination
+from api.permissions import IsAuthenticatedUser
 from api.serializers.recipes import RecipeSerializer, RecipeCreateSerializer
 from recipes.models import Recipe
 
@@ -10,6 +11,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     queryset = Recipe.objects.all()
     pagination_class = LimitPageNumberPagination
+    permission_classes = (IsAuthenticatedUser,)
 
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
