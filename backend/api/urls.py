7d4788881db32a262db2a83344554783277e1d73
Vladislav Gerasimov <Vladislav.Gerasimov@kaspersky.com>
2022-10-28 23:48:17 +0300

    Реализация скачивания списка покупок

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from api.views.favorite import FavoriteViewSet
 from api.views.ingredients import IngredientViewSet
 from api.views.recipes import RecipeViewSet
-from api.views.shopping_cart import ShoppingCartViewSet
+from api.views.shopping_cart import ShoppingCartViewSet, ShoppingCartDownloadView
 from api.views.subscriptions import SubscribeViewSet, SubscriptionsViewSet
 from api.views.tags import TagViewSet
 
@@ -28,11 +28,15 @@ router.register(
     r'recipes/(?P<id>[\d]+)/shopping_cart',
     ShoppingCartViewSet, basename='shopping_cart'
 )
-
 app_name = 'api'
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(
+        'recipes/download_shopping_cart/',
+        ShoppingCartDownloadView.as_view(),
+        name='download_shopping_cart'
+    ),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),
 ]
--- backend/api/views/shopping_cart.py
+++ backend/api/views/shopping_cart.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, status
+from django.http import FileResponse, HttpResponse
+from rest_framework import viewsets, status, renderers, views
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from api.permissions import IsAuthenticatedUser
+from api.serializers.recipes import RecipeSerializer, IngredientInRecipeSerializer
 from api.serializers.shopping_cart import ShoppingCartSerializer
 from recipes.models import Recipe, ShoppingCart
 
@@ -24,3 +26,31 @@ class ShoppingCartViewSet(viewsets.ModelViewSet):
             data={'detail': 'Рецепт не добавлен в избранное!'},
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class ShoppingCartDownloadView(views.APIView):
+    """Вью для скачивания списка покупок."""
+    permission_classes = (IsAuthenticatedUser,)
+
+    def prepare_shopping_cart(self):
+        ingredients_recipes = [
+            unit.recipe.ingredient_amount.all() for unit in ShoppingCart.objects.filter(user=self.request.user)
+        ]
+        ingredients_amount = [ing for x in ingredients_recipes for ing in x]
+        shopping_ingredients = {}
+        for ingredient_amount in ingredients_amount:
+            ingredient_name = (
+                f'{str(ingredient_amount.ingredient).capitalize()} ({ingredient_amount.ingredient.measurement_unit})'
+            )
+            if shopping_ingredients.get(ingredient_name):
+                shopping_ingredients[ingredient_name] += ingredient_amount.amount
+            else:
+                shopping_ingredients[ingredient_name] = ingredient_amount.amount
+        return shopping_ingredients
+
+    def get(self, *args, **kwargs):
+        shopping_ingredients = self.prepare_shopping_cart()
+        shopping_list = '\n'.join([f'{name} - {amount}' for name, amount in shopping_ingredients.items()])
+        response = HttpResponse(shopping_list, content_type='text/plain; charset=utf8')
+        response['Content-Disposition'] = f'attachment; filename="shopping_list.txt"'
+        return response
