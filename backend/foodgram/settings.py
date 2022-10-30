0a3282fcc0b11411617ed7d50555d76c1bc1e6e3
Vladislav Gerasimov <Vladislav.Gerasimov@kaspersky.com>
2022-10-30 13:03:09 +0300

    Добавление workflow для автоматической сборки и отправки образа

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(strtobool(os.getenv('DEBUG', default=True)))
+DEBUG = bool(strtobool(os.getenv('DEBUG', default='True')))
 
 ALLOWED_HOSTS = ['*']
 
